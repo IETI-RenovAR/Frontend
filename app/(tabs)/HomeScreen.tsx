37726d7a1c81143b3423b5cc5d4851199a420b3e
Ana Duran <ana.duran-b@mail.escuelaing.edu.co>
2025-05-06 20:41:14 -0500

    disenio pagina principal

---
+++ app/(tabs)/HomeScreen.tsx
@@ -0,0 +1,153 @@
+import React, { JSX, useState } from 'react';
+import {
+  GestureResponderEvent,
+  Image,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+export default function HomeScreen(): JSX.Element {
+  const [activeTab, setActiveTab] = useState<'Spaces' | 'Things'>('Spaces');
+
+  const handlePress = (event: GestureResponderEvent): void => {
+    console.log('Pressed item');
+  };
+
+  return (
+    <View style={styles.container}>
+      {/* Header */}
+      <View style={styles.header}>
+        <Text style={styles.greeting}>Good morning, Laura!</Text>
+        <Text style={styles.subGreeting}>what do you want to do today?</Text>
+      </View>
+
+      {/* Tabs */}
+      <View style={styles.tabs}>
+        <TouchableOpacity onPress={() => setActiveTab('Spaces')}>
+          <Text style={[styles.tabText, activeTab === 'Spaces' ? styles.activeTab : styles.inactiveTab]}>
+            Spaces
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => setActiveTab('Things')}>
+          <Text style={[styles.tabText, activeTab === 'Things' ? styles.activeTab : styles.inactiveTab]}>
+            Things
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Content by Tab */}
+      <View style={styles.projectContainer}>
+        {activeTab === 'Spaces' ? (
+            <>
+          <TouchableOpacity onPress={handlePress} style={styles.projectButton}>
+            <Image
+              source={require('../../assets/images/bedroom.png')}
+              style={styles.image}
+            />
+            <Text style={styles.projectText}>Bedroom</Text>
+          </TouchableOpacity>
+          <TouchableOpacity onPress={handlePress} style={styles.projectButton}>
+          <Image
+            source={require('../../assets/images/kitchen.png')}
+            style={styles.image}
+          />
+          <Text style={styles.projectText}>Kitchen</Text>
+        </TouchableOpacity>
+          
+        </>
+        ) : (
+          <>
+            <TouchableOpacity onPress={handlePress} style={styles.projectButton}>
+              <Image
+                source={require('../../assets/images/chair.png')}
+                style={styles.image}
+              />
+              <Text style={styles.projectText}>Chair</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={handlePress} style={styles.projectButton}>
+              <Image
+                source={require('../../assets/images/table.png')}
+                style={styles.image}
+              />
+              <Text style={styles.projectText}>Table</Text>
+            </TouchableOpacity>
+          </>
+        )}
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#FFF4E9',
+  },
+  header: {
+    backgroundColor: '#4B3425',
+    borderBottomLeftRadius: 40,
+    borderBottomRightRadius: 40,
+    padding: 30,
+    paddingTop: 60,
+    height:200,
+  },
+  greeting: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#ffffff',
+    paddingTop: 25,
+  },
+  subGreeting: {
+    fontSize: 16,
+    color: '#dddddd',
+    marginTop: 4,
+  },
+  tabs: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    marginTop: 30,
+    marginBottom: 10,
+  },
+  tabText: {
+    fontSize: 18,
+    paddingBottom: 4,
+  },
+  activeTab: {
+    color: '#6B4B3A',
+    borderBottomColor: '#6B4B3A',
+    borderBottomWidth: 2,
+  },
+  inactiveTab: {
+    color: '#aaaaaa',
+  },
+  projectContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    marginTop: 20,
+    paddingHorizontal: 10,
+  },
+  projectButton: {
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    resizeMode: 'contain',
+  },
+  projectText: {
+    marginTop: 8,
+    fontSize: 16,
+    color: '#3C3C3C',
+  },
+  footer: {
+    position: 'absolute',
+    bottom: 20,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+});
